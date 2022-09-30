552b7721d2864218f1e9526e4076d75ca8024f1c
eytong56 <67028470+eytong56@users.noreply.github.com>
2022-09-30 15:46:45 -0400

    Change links from relative to direct
    Changed all html links in navbar.js to cmujsa.com links to remove html extension

--- js/navbar.js
+++ js/navbar.js
@@ -3,7 +3,7 @@ const navbarItems =
 <!-- Navigation Bar START -->
 <nav class="navbar navbar-expand-lg sticky-top justify-content-center m-0 p-0">
     <div class="container-fluid p-0">
-        <a class="navbar-brand hide-desktop" href="./index.html">
+        <a class="navbar-brand hide-desktop" href="https://www.cmujsa.com">
             <div class="wrap">
                 <div>
                     <img src="images/jsalogo1.png" class="img-fluid nav-img" alt="JSA Logo">
@@ -21,7 +21,7 @@ const navbarItems =
             <ul class="navbar-nav">
                 <li class="nav-item first hide-mid"></li>
                 <li class="nav-item first-mid hide-mobile">
-                    <a class="nav-link" href="./index.html">
+                    <a class="nav-link" href="https://www.cmujsa.com">
                         <div class="wrap">
                             <div>
                                 <img src="images/jsalogo1.png" class="img-fluid nav-img" alt="JSA Logo">
@@ -33,23 +33,23 @@ const navbarItems =
                     </a>
                 </li>
                 <li class="nav-item hide-desktop">
-                    <a class="nav-link" href="./index.html">Home<br>ホーム</a>
+                    <a class="nav-link" href="https://www.cmujsa.com">Home<br>ホーム</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="./members.html">Members<br>会員</a>
+                    <a class="nav-link" href="https://www.cmujsa.com/members">Members<br>会員</a>
                 </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link " href="" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                         Events<br>イベント
                     </a>
                     <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item" href="./cultural.html">Cultural Events<br>文化イベント</a></li>
-                        <li><a class="dropdown-item" href="./matsuri.html">Matsuri<br>祭り</a></li>
-                        <li><a class="dropdown-item" href="./booth.html">Booth<br>ブース</a></li>
+                        <li><a class="dropdown-item" href="https://www.cmujsa.com/cultural">Cultural Events<br>文化イベント</a></li>
+                        <li><a class="dropdown-item" href="https://www.cmujsa.com/matsuri">Matsuri<br>祭り</a></li>
+                        <li><a class="dropdown-item" href="https://www.cmujsa.com/booth">Booth<br>ブース</a></li>
                     </ul>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="./faq.html">FAQ<br>良くある質問 </a>
+                    <a class="nav-link" href="https://www.cmujsa.com/faq">FAQ<br>良くある質問 </a>
                 </li>
                 <li class="nav-item hide-mid"></li>
             </ul>
