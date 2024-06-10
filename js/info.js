27fe4463282000fc14b239ccd4b8bba640d2af97
eytong56 <67028470+eytong56@users.noreply.github.com>
2024-06-10 19:28:05 -0400

    Update README.md to add directions on how to update the website

--- js/info.js
+++ js/info.js
@@ -1,6 +1,6 @@
 // EDIT THE FOLLOWING TWO VARIABLES WHEN A NEW BOARD IS ELECTED
-const mainContactName = "May Sonoda"; // Will be used in Q&A
-const mainContactAndrewID = "maysonod"; // Will be used in Q&A and footer
+const mainContactName = "May Sonoda"; // Will be used in FAQ
+const mainContactAndrewID = "maysonod"; // Will be used in FAQ and footer
 
 // DON'T EDIT THIS
 class ExecMember{
